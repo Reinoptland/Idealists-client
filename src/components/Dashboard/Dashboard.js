baf1cdca8c4a5453ff600676ca2320ecc8c4e5e2
Dario <salina.dario@gmail.com>
2019-03-11 12:15:52 +0100

    id not match any file(s) known ➜

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,57 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import request from 'superagent';
-import { baseUrl } from '../../constants'
-import { Redirect } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import request from "superagent";
+import { baseUrl } from "../../constants";
+import { Redirect } from "react-router-dom";
 
 export default function Dashboard(props) {
-    const jwt = localStorage.currentUserJwt
-    const [userData, setUserData] = useState({})
-    const [userIdeas, setUserIdeas] = useState([])
-    const [userLoggedIn, setUserLoggedIn] = useState(true)
+  const jwt = localStorage.currentUserJwt;
+  const [userData, setUserData] = useState({});
+  // const [userIdeas, setUserIdeas] = useState([])
+  const [userLoggedIn, setUserLoggedIn] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
+  // look into useContext instead
+  useEffect(() => {
+    setIsLoading(true);
+    request
+      .get(`${baseUrl}/current`)
+      .set("Authorization", `Bearer ${jwt}`)
+      .then(res => setUserData(res.body))
+      .then(() => console.log("dataFetched"))
+      .then(() => setIsLoading(false));
+  }, []);
 
+  // This should get ideas by industry
+  // useEffect(() => {
+  //     request
+  //         .get(`${baseUrl}/ideas/:industry`)
 
-    // look into useContext instead
-    useEffect(() => {
-        request
-            .get(`${baseUrl}/current`)
-            .set("Authorization", `Bearer ${jwt}`)
-            .then(res => setUserData(res.body))
-    }, [])
+  // }, [userData])
 
+  const userLogout = () => {
+    localStorage.removeItem("currentUserJwt");
+    setUserLoggedIn(false);
+  };
 
-    // This should get ideas by industry
-    // useEffect(() => {
-    //     request
-    //         .get(`${baseUrl}/ideas/:industry`)
+  if (userLoggedIn === false) return <Redirect to="/login" />;
 
-    // }, [userData])
-
-    const userLogout = () => {
-        localStorage.removeItem('currentUserJwt')
-        setUserLoggedIn(false)
-    }
-
-    if (userLoggedIn === false)
-        return (
-            <Redirect to="/login" />)
-
-    return (
+  return (
+    <div>
+      {isLoading ? (
+        <div>Loading, please refresh</div>
+      ) : (
         <div>
-
-            <h4>This is {userData.firstName}'s dashboard</h4>
-            <button onClick={userLogout}>Log out</button>
-            <h3>Edit my profile</h3>
-            <h1>Dashboard</h1>
-            <ul>
-                <li>Sample</li>
-                <li>Data</li>
-                <li>With</li>
-                <li>Idea</li>
-                <li>Links</li>
-            </ul>
-
+          <h4>This is {userData.firstName}'s dashboard</h4>
+          <button onClick={userLogout}>Log out</button>
+          <h3>Edit my profile</h3>
+          <h1>Dashboard</h1>
+          <ul>
+            <li>Sample</li>
+            <li>Data</li>
+            <li>With</li>
+            <li>Idea</li>
+            <li>Links</li>
+          </ul>
         </div>
-    )
-
+      )}
+    </div>
+  );
 }
